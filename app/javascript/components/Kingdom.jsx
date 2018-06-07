d6221cff8ce8b1bb94a5ea0a0dc08e31982def4a
Derek Kniffin <derek.kniffin@gmail.com>
2018-06-06 21:25:00 -0400

    Update Kingdom component to show correctly in form

--- app/javascript/components/Kingdom.jsx
+++ app/javascript/components/Kingdom.jsx
@@ -8,21 +8,26 @@ const propTypes = {
 };
 
 function Kingdom({ kingdom }) {
-  const ratingString = kingdom.rating ? (new Array(kingdom.rating + 1)).join('* ').trim() : '';
+  // TODO: Display the rating as stars instead of asterisks
+  const ratingString = kingdom.rating ?
+    (new Array(kingdom.rating + 1)).join('* ').trim() :
+    'TBD';
+  const titleString = kingdom.name || 'New Kingdom';
+  const playCountString = kingdom.playCount || 0;
+
   return (
     <div className="c-kingdom">
       <div className="c-kingdom__header">
-        <span className="c-kingdom__title">{kingdom.name}</span>
+        <span className="c-kingdom__title">{titleString}</span>
 
         <div className="c-kingdom__rating">
           <div className="c-kingdom__label">Avg Rating</div>
-          {/* TODO: Display this as stars instead of asterisks */}
           <span>{ratingString}</span>
         </div>
         <div className="c-kingdom__play-count">
           <div className="c-kingdom__label">Play Count</div>
           {/* TODO: Save play count on the backend, and display that here */}
-          <span>100</span>
+          <span>{playCountString}</span>
         </div>
       </div>
       <div className="c-kingdom__cards">
