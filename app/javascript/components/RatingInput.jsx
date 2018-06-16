701f69f6d63d2a275ae25e69ef95752ebc291c16
Derek Kniffin <derek.kniffin@gmail.com>
2018-06-15 23:56:48 -0400

    Set up icons for rating input

--- app/javascript/components/RatingInput.jsx
+++ app/javascript/components/RatingInput.jsx
@@ -1,12 +1,24 @@
+import PropTypes from 'prop-types';
 import React from 'react';
-
 import ReactStarRating from 'react-star-rating-component';
 
-const propTypes = { };
+import UserIcon from 'components/UserIcon';
+import StarIcon from 'components/StarIcon';
+
+const propTypes = {
+  icon: PropTypes.string
+};
 
 function RatingInput(props) {
+  const Icon = props.icon === 'user' ? UserIcon : StarIcon;
   return (
-    <ReactStarRating {...props} />
+    <ReactStarRating
+      {...props}
+      starColor="#dc8c46"
+      renderStarIcon={() => {
+        return <div style={{ width: 20, height: 20 }}><Icon /></div>;
+      }}
+    />
   );
 }
 
