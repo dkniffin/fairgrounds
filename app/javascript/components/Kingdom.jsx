5c1275ae4ac43ba3f192f42c134660d1820c18d3
Derek Kniffin <derek.kniffin@gmail.com>
2018-06-19 07:53:21 -0400

    Style kingdom show page

--- app/javascript/components/Kingdom.jsx
+++ app/javascript/components/Kingdom.jsx
@@ -1,6 +1,5 @@
 import PropTypes from 'prop-types';
 import React from 'react';
-import classnames from 'classnames';
 
 import { kingdomType } from 'types';
 import Card from 'components/Card';
@@ -9,10 +8,14 @@ import Routes from 'routes';
 
 const propTypes = {
   kingdom: kingdomType,
-  small: PropTypes.bool
+  showHeader: PropTypes.bool
 };
 
-function Kingdom({ kingdom, small }) {
+const defaultProps = {
+  showHeader: true
+};
+
+function Kingdom({ kingdom, showHeader }) {
   // TODO: Display the rating as stars instead of asterisks
   const ratingString = kingdom.rating ?
     (new Array(kingdom.rating + 1)).join('* ').trim() :
@@ -20,26 +23,23 @@ function Kingdom({ kingdom, small }) {
   const titleString = kingdom.name || 'New Kingdom';
   const playCountString = kingdom.playCount || 0;
 
-  const kingdomClasses = classnames({
-    'c-kingdom': true,
-    'c-kingdom--small': small
-  });
-
   return (
-    <div className={kingdomClasses}>
-      <div className="c-kingdom__header">
-        <a className="c-kingdom__title" href={Routes.kingdomPath(kingdom.id)}>{titleString}</a>
+    <div className='c-kingdom'>
+      {showHeader &&
+        <div className="c-kingdom__header">
+          <a className="c-kingdom__title" href={Routes.kingdomPath(kingdom.id)}>{titleString}</a>
 
-        <div className="c-kingdom__rating">
-          <div className="c-kingdom__label">Avg Rating</div>
-          <span>{ratingString}</span>
+          <div className="c-kingdom__rating">
+            <div className="c-kingdom__label">Avg Rating</div>
+            <span>{ratingString}</span>
+          </div>
+          <div className="c-kingdom__play-count">
+            <div className="c-kingdom__label">Play Count</div>
+            {/* TODO: Save play count on the backend, and display that here */}
+            <span>{playCountString}</span>
+          </div>
         </div>
-        <div className="c-kingdom__play-count">
-          <div className="c-kingdom__label">Play Count</div>
-          {/* TODO: Save play count on the backend, and display that here */}
-          <span>{playCountString}</span>
-        </div>
-      </div>
+      }
       <div className="c-kingdom__cards">
         {kingdom.cards.map((cardData) => {
           return (
@@ -54,6 +54,7 @@ function Kingdom({ kingdom, small }) {
   );
 }
 
+Kingdom.defaultProps = defaultProps;
 Kingdom.propTypes = propTypes;
 
 export default Kingdom;
