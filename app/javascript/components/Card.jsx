14030bd93721af9e3063f87e49e85d6633ac28ad
Derek Kniffin <derek.kniffin@gmail.com>
2018-08-10 22:52:51 -0400

    Add optional X button to Card component

--- app/javascript/components/Card.jsx
+++ app/javascript/components/Card.jsx
@@ -1,13 +1,22 @@
+import PropTypes from 'prop-types';
 import React from 'react';
 import { cardType } from 'types';
 
 const propTypes = {
-  cardData: cardType
+  cardData: cardType,
+  onClose: PropTypes.func
 };
 
-function Card({ cardData }) {
+function Card({ cardData, onClose }) {
   return (
     <div className="c-card">
+      {onClose &&
+        <button
+          className="c-card__x-button"
+          onClick={() => onClose(cardData)}
+        >
+          &#215;
+        </button>}
       <img src={cardData.imagePath} />
     </div>
   );
